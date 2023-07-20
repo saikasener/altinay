7a15d0cd933221e77cc9268899b5bdc8d10c44b0
saikasener <saikasener1@gmail.com>
2023-07-20 11:27:25 +0300

    Trying to get position accurately

--- ros_openimu/scripts/imu_gyro.py
+++ ros_openimu/scripts/imu_gyro.py
@@ -5,121 +5,134 @@ from geometry_msgs.msg import Vector3, Twist
 from scipy.integrate import cumtrapz
 import time
 import numpy as np
+from scipy import signal
+from scipy.optimize import curve_fit
+
+start_bool = True 
 
 
-class IMUGyro:
-    def __init__(self):
-        rospy.init_node('imu_accuracy_tester')
-        self.linear_acceleration = Vector3()
-        self.angular_velocity = Vector3()
-        self.position = Vector3()
-        self.orientation = Vector3()
-        self.previous_time = rospy.get_time()
-
-        self.integ1_array_x=0
-        self.integ1_array_y=0
-        self.integ1_array_z=0 
-
-        self.integ1_array =[]
-        self.imu_array = []
-
-        self.ax = 0
-        self.ay = 0
-        self.az = 0
-        self.dif=0
-        self.cal_size = 500  # points to use for calibration
-        rospy.Subscriber('/imu_acc_ar', Imu, self.get_gyro)
-        self.orientation_pub = rospy.Publisher('/imu/orientation', Vector3, queue_size=1)
-
-
-    def get_gyro(self, data):
-        dt = rospy.get_time() - self.previous_time      
-        self.ax = data.linear_acceleration.x
-        self.ay = data.linear_acceleration.y
-        self.az = data.linear_acceleration.z
-        self.gyro_cal()
-        return self.ax,self.ay,self.az
-        # self.update_angular_position(dt)
-
-    def gyro_cal(self):
-        # [self.get_gyro() for ii in range(0,self.cal_size)] # clear buffer before calibration
-        # mpu_array = []
-        # gyro_offsets = [0.0,0.0,0.0]
-        # while True:
-        #     try:
-        #         self.wx,self.wy,self.wz = self.get_gyro() # get gyro vals
-        #     except:
-        #         continue
-
-        #     mpu_array.append([self.wx,self.wy,self.wz])
-
-        #     if np.shape(mpu_array)[0]==self.cal_size:
-        #         for qq in range(0,3):
-        #             gyro_offsets[qq] = np.mean(np.array(mpu_array)[:,qq]) # average
-        #         break
-        # print('Gyro Calibration Complete')
-        # return gyro_offsets
-
-       
-        # self.imu_array.append([self.ax,self.ay,self.az])
-        if self.ax > 0.001 and self.ay< 0.001:
-            self.integrate_acc(self.ax,0,0)
-        elif self.ay > 0.001 and self.ax< 0.001:
-            self.integrate_acc(0,self.ay,0)
-        elif self.ax< 0.001 and self.ay < 0.001:
-            self.integrate_acc(0,0,self.az)
-        elif self.ax > 0.001 and self.ay > 0.001:
-            self.integrate_acc(self.ax,self.ay,0)  
-                      
-        else:
-            print("-"*20)
-
-    def integrate_acc(self,ax,ay,az):
-        # print ("ax : ",ax, "             ay : ", ay)
-# -------------------------------------------------
-        x = np.arange(0, 10)
-        y = np.arange(0, 10)
-        
-
-    def update_angular_position(self,dt):
-        gyro_offsets = self.gyro_cal() # calculate gyro offsets
-        data,t_vec = [],[]
-        t0 = time.time()
-        while time.time()-t0<dt:
-            data.append(self.get_gyro())
-            t_vec.append(time.time()-t0)
-
-        rot_axis_x = 0 
-        data_offseted_x = np.array(data)[:,rot_axis_x]-gyro_offsets[rot_axis_x]
-        self.integ1_array_x = cumtrapz(data_offseted_x,x=t_vec) 
-
-        rot_axis_y = 1 
-        data_offseted_y = np.array(data)[:,rot_axis_y]-gyro_offsets[rot_axis_y]
-        self.integ1_array_y = cumtrapz(data_offseted_y,x=t_vec) 
-        
-        rot_axis_z = 2 
-        data_offseted_z = np.array(data)[:,rot_axis_z]-gyro_offsets[rot_axis_z]
-        self.integ1_array_z = cumtrapz(data_offseted_z,x=t_vec)
-
-        self.orientation = [self.integ1_array_x,self.integ1_array_y,self.integ1_array_z]
-        self.orientation_pub.publish(self.orientation)
-
-    #     dt = rospy.get_time() - self.previous_time
-        # self.angular_position.x += self.angular_velocity.x * dt 
-        # self.angular_position.y += self.angular_velocity.y * dt 
-        # self.angular_position.z += self.angular_velocity.z * dt 
-        # self.previous_time = rospy.get_time()
-
+def get_accel(data):
+    global array_data,ax,ay,az
+    ax=data.linear_acceleration.x
+    ay=data.linear_acceleration.y
+    az=data.linear_acceleration.z
+    array_data = np.array([ax,ay,az])
 
     
 
+def imu_integrator():
+    global array_data,ax,ay,az
+    data_indx = 1 # index of variable to integrate
+    dt_stop = 0.01 # seconds to record and integrate
 
+    break_bool = False
 
-    def run(self):
-        # self.previous_time = rospy.get_time()
-        # self.loop_rate.sleep()
-        rospy.spin()
+    accel_array,t_array = [],[]
+    print("Starting Data Acquisition")
+    t0 = time.time()
+    loop_bool = False
+    while True:
+        try:
+            t_array.append(time.time()-t0)
+            data_array = [ax,ay,az]
+            accel_array.append(accel_fit(data_array[data_indx],
+                                            *accel_coeffs[data_indx]))
+            if not loop_bool:
+                loop_bool = True
+                print("Start Moving IMU...")
+        except:
+            continue
+        if time.time()-t0>dt_stop:
+            print("Data Acquisition Stopped")
+            break
+            
+        if break_bool:
+            break
+
+        # Fs_approx = len(accel_array)/dt_stop
+        # b_filt,a_filt = signal.butter(4,5,'low',fs=Fs_approx)
+        # accel_array = signal.filtfilt(b_filt,a_filt,accel_array)
+        # accel_array = np.multiply(accel_array,9.80665)
+        #
+        ##################################
+        # Print Sample Rate and Accel
+        # Integration Value
+        ##################################
+        #
+        # print("Sample Rate: {0:2.0f}Hz".format(len(accel_array)/dt_stop))
+        veloc_array = np.append(0.0,cumtrapz(accel_array,x=t_array))
+        dist_approx = np.trapz(veloc_array,x=t_array)
+        dist_array = np.append(0.0,cumtrapz(veloc_array,x=t_array))
+        # print(dist_array)
+#
+def accel_fit(x_input,m_x,b):
+    return (m_x*x_input)+b # fit equation for accel calibration
+#
+
+def accel_cal():
+    global ax,ay,az
+    print("-"*50)
+    print("Accelerometer Calibration")
+    mpu_offsets = [[],[],[]] # offset array to be printed
+    axis_vec = ['z','y','x'] # axis labels
+    cal_directions = ["upward","downward","perpendicular to gravity"] # direction for IMU cal
+    cal_indices = [2,1,0] # axis indices
+    for qq,ax_qq in enumerate(axis_vec):
+        ax_offsets = [[],[],[]]
+        print("-"*50)
+        for direc_ii,direc in enumerate(cal_directions):
+            input("-"*8+" Press Enter and Keep IMU Steady to Calibrate the Accelerometer with the -"+\
+              ax_qq+"-axis pointed "+direc)
+            [array_data for ii in range(0,cal_size)] # clear buffer between readings
+            mpu_array = []
+            while len(mpu_array)<cal_size:
+                try:
+                    mpu_array.append([ax,ay,az]) # append to array
+                except:
+                    continue
+            ax_offsets[direc_ii] = np.array(mpu_array)[:,cal_indices[qq]] # offsets for direction
+
+        # Use three calibrations (+1g, -1g, 0g) for linear fit
+        popts,_ = curve_fit(accel_fit,np.append(np.append(ax_offsets[0],
+                                 ax_offsets[1]),ax_offsets[2]),
+                   np.append(np.append(1.0*np.ones(np.shape(ax_offsets[0])),
+                    -1.0*np.ones(np.shape(ax_offsets[1]))),
+                        0.0*np.ones(np.shape(ax_offsets[2]))),
+                            maxfev=10000)
+        mpu_offsets[cal_indices[qq]] = popts # place slope and intercept in offset array
+    print('Accelerometer Calibrations Complete')
+    time.sleep(60)
+    return mpu_offsets
 
 if __name__ == '__main__':
-    imu_accuracy_tester = IMUGyro()
-    imu_accuracy_tester.run()
+    rospy.init_node('imu_accuracy_tester')
+    linear_acceleration = Vector3()
+    angular_velocity = Vector3()
+    position = Vector3()
+    orientation = Vector3()
+    previous_time = rospy.get_time()
+
+    integ1_array_x=0
+    integ1_array_y=0
+    integ1_array_z=0 
+
+    integ1_array =[]
+
+    array_data=[]
+    ax = 0
+    ay = 0
+    az = 0
+    dif=0
+    cal_size = 500  # points to use for calibration
+    accel_coeffs = [np.array([ 0.9978107 , -0.19471673]),
+                            np.array([ 0.99740193, -0.56129248]),
+                            np.array([0.9893495 , 0.20589886])]
+    
+    rospy.Subscriber('/imu_acc_ar', Imu,  get_accel)
+    orientation_pub = rospy.Publisher('/imu/position', Vector3, queue_size=1)
+    
+    imu_integrator()
+    
+    rate = rospy.Rate(100)
+    while not rospy.is_shutdown():
+        rate.sleep()
--- ros_openimu/scripts/imu_testpy2.py
+++ ros_openimu/scripts/imu_testpy2.py
@@ -3,42 +3,100 @@
 import rospy
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Vector3
+from std_msgs.msg import Float32MultiArray
+import numpy as np
+from scipy.integrate import cumtrapz,trapz
+from rospy.numpy_msg import numpy_msg
+from rospy_tutorials.msg import Floats
+
 
 class IMUAccuracyTester:
     def __init__(self):
         rospy.init_node('imu_accuracy_tester')
-        self.linear_acceleration = Vector3()
+        self.linear_acceleration = []
         self.angular_velocity = Vector3()
         self.position = Vector3()
         self.orientation = Vector3()
         self.previous_time = rospy.get_time()
         self.loop_rate = rospy.Rate(50)
-        
-
+        self.final_position = []              
+        self.acceleration=[]
         self.position.x = 0
         self.position.y = 0
         self.position.z = 0
+        self.final_position_msg=Vector3()
+        
+        
+        self.sayac=0
 
         rospy.Subscriber('/imu_acc_ar', Imu, self.imu_callback)
         self.position_pub = rospy.Publisher('/imu/position', Vector3, queue_size=10)
-        self.orientation_pub = rospy.Publisher('/imu/orientation', Vector3, queue_size=10)
+        # self.orientation_pub = rospy.Publisher('/imu/orientation', Vector3, queue_size=10)
 
 
     def imu_callback(self, data):
-        dt = rospy.get_time() - self.previous_time      
-        self.linear_acceleration.x = data.linear_acceleration.x
-        self.linear_acceleration.y = data.linear_acceleration.y
-        self.linear_acceleration.z = data.linear_acceleration.z
+              
+        self.linear_acceleration.append ([data.linear_acceleration.x,data.linear_acceleration.y,data.linear_acceleration.z])
+        self.acceleration = np.asarray(self.linear_acceleration)
+        self.integrate_acc(self.acceleration)
         
-        self.update_position(dt)
+        # self.angular_velocity.x = data.angular_velocity.x
+        # self.angular_velocity.y = data.angular_velocity.y
+        # self.angular_velocity.z = data.angular_velocity.z
+        # self.update_orientation(dt)
+
+    def integrate_acc(self,acceleration):
+        dt = rospy.get_time() - self.previous_time        
+
+        velocity = cumtrapz(acceleration, axis=0, dx=dt,initial=0)
+        self.final_position = cumtrapz(velocity, axis=0, dx=dt,initial=0)
         
-        self.angular_velocity.x = data.angular_velocity.x
-        self.angular_velocity.y = data.angular_velocity.y
-        self.angular_velocity.z = data.angular_velocity.z
+        ax= self.final_position[self.sayac,0]
+        ay= self.final_position[self.sayac,1]
+        az= self.final_position[self.sayac,2]
+        self.update_position(ax,ay,az)
+        # if ax > 0.01 and ay< 0.01:
+        #     self.update_position(ax,0,0)
+        #     print("ax > 0.001 and ay< 0.001")
+        # elif ay > 0.001 and ax< 0.001:
+        #     self.update_position(0,ay,0)
+        # elif ax< 0.001 and ay < 0.001:
+        #     self.update_position(0,0,az)
+        # elif ax > 0.001 and ay > 0.001:
+        #     self.update_position(ax,ay,0)                        
+        # else:
+        #     print("-"*20)
 
-        self.update_orientation(dt)
+        self.sayac += 1
+        # self.update_position(self.acceleration,dt)
+
+    def position_formula(self):
+        #trying to use distance formula  
+        dt = rospy.get_time() - self.previous_time
+        self.linear_velocity.x += self.linear_acceleration.x * dt
+        self.linear_velocity.y += self.linear_acceleration.y * dt
+        self.linear_velocity.z += self.linear_acceleration.z * dt
+
+        self.linear_position.x += self.linear_velocity.x * dt 
+        self.linear_position.y += self.linear_velocity.y * dt 
+        self.linear_position.z += self.linear_velocity.z * dt 
+        self.distance_lin_pub.publish(self.linear_position)
+
+        self.angular_position.x += self.angular_velocity.x * dt 
+        self.angular_position.y += self.angular_velocity.y * dt 
+        self.angular_position.z += self.angular_velocity.z * dt 
+        self.previous_time = rospy.get_time()
+        self.distance_ang_pub.publish(self.angular_position)
+
+    
+    def update_position(self,ax,ay,az):
+        self.final_position_msg.x= ax
+        self.final_position_msg.y= ay
+        self.final_position_msg.z= az
+        self.position_pub.publish(self.final_position_msg)
 
 
+       
     def update_orientation(self, dt):
         self.orientation.x += self.angular_velocity.x * dt
         self.orientation.y += self.angular_velocity.y * dt
